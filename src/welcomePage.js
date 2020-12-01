6b54c4d54564e183236a4c7d05edfb27d5b95838
BenjaminIglesias <cph-bc106@cphbusiness.dk>
2020-12-01 15:14:10 +0100

    storing temp changes

--- src/welcomePage.js
+++ src/welcomePage.js
@@ -1,17 +1,26 @@
 export default function Content(){
    return(
-       <div>
-           <h3>Group 4: Benjamin Iglesias, Mads Bech, Patrick Jahn</h3>
-           <br></br>
-           <h3>To use this project locally please do the following:</h3>
-           <h3> Git clone the backend and run it (we recommend using netbeans), refer to README.md for instructions</h3>
-           <p>https://github.com/PatrickJahn/ca3</p>
-           <h3>Then navigate to settings.js and change the variable URL to the locally used URL  </h3>
-           <h3>Now u can attempt to log in. If a response appears, the connection to the backend should be correct </h3>
-           <h3>If no response appears, check console (normally throws an false CORS-error) and check that ur backend is running</h3>
-           <br></br>
-           <h3>Further instructions on using the frontend of this project can be found here in README.md</h3>
-           <p>https://github.com/BenjaminIglesias/CA3-Frontend</p>
-       </div>
-   ) 
-}
+<div>
+    <h1 style={{textAlign:"center", fontFamily: "cursive" }} >Velkommen til FoodSaver</h1>
+    <h3 style={{textAlign:"center", fontFamily: "cursive"}}>Indtast dit postnummer nedenunder</h3>
+<div class="block"><input type="text" class="input-res"  maxlength="4"/><a class="btn sky block circular">Vis</a>
+</div>
+
+<br/>
+<h3 style={{textAlign:"left", fontFamily: "cursive",  transform: "rotate(15deg)"
+}}>For din pengepungs og miljøets skyld!</h3>
+
+<h3 style={{textAlign:"right", fontFamily: "cursive",  transform: "rotate(-15deg)"
+}}>Datovarer fra din lokale Netto, Føtex eller Bilka butik</h3>
+
+
+<h3 style={{textAlign:"center", fontFamily: "cursive"
+}}>Gennemsnitlig besparelse på 50%!</h3>
+</div>
+    
+
+
+    ) 
+   }
+
+   
